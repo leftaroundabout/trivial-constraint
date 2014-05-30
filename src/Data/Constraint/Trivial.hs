801be161d6d28cac4d53f54fdbe6809a17f17f94
Justus Sagemüller <sagemuej@smail.uni-koeln.de>
2014-05-30 12:42:43 +0200

    'Trivial' is a pretty bad name, changed it to 'Unconstrained'.
    While nobody depends on the package yet...

--- src/Data/Constraint/Trivial.hs
+++ src/Data/Constraint/Trivial.hs
@@ -11,7 +11,7 @@ module Data.Constraint.Trivial where
 -- | Intended to be used as an argument for some type constructor which expects kind
 --   @* -> Constraint@, when you do not actually wish to constrain anything with it.
 --   
---   @'Trivial' t@ can always be added to the constraint list of any signature, without
+--   @'Unconstrained' t@ can always be added to the constraint list of any signature, without
 --   changing anything.
-class Trivial t
-instance Trivial t
+class Unconstrained t
+instance Unconstrained t
