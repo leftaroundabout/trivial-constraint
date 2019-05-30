4949ba17162515cf766225f963aa6910586b4138
Justin Le <justin@jle.im>
2019-05-29 17:46:33 -0700

    new Impossible mechanism that allows partial application

--- src/Data/Constraint/Trivial.hs
+++ src/Data/Constraint/Trivial.hs
@@ -4,10 +4,12 @@
 -- License     :  GPL v3 (see LICENSE)
 -- Maintainer  :  (@) jsagemue $ uni-koeln.de
 --
-{-# LANGUAGE ConstraintKinds       #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE ConstraintKinds         #-}
+{-# LANGUAGE FlexibleInstances       #-}
+{-# LANGUAGE MultiParamTypeClasses   #-}
+{-# LANGUAGE PolyKinds               #-}
+{-# LANGUAGE UndecidableInstances    #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
 
 module Data.Constraint.Trivial (
             Unconstrained, Impossible
@@ -21,6 +23,8 @@ module Data.Constraint.Trivial (
           , Unconstrained9, Impossible9
           ) where
 
+import           GHC.Exts (Any)
+
 -- | Intended to be used as an argument for some type constructor which expects kind
 --   @k -> Constraint@, when you do not actually wish to constrain anything with it.
 --
@@ -33,9 +37,11 @@ instance Unconstrained t
 -- | This constraint can /never/ be fulfilled. Might be useful e.g. as a default
 --   for a class-associated constraint; this basically disables any method with
 --   that constraint (so it can safely be left 'undefined').
-type Impossible t = HiddenEmptyClass t
-
-class HiddenEmptyClass t
+--
+--   This implementation works by requiring any instance also satify the
+--   'Any' constraint, but the 'Any' constraint is impossible to ever
+--   fulfil.
+class Any => Impossible t
 
 
 -- | Like 'Unconstrained', but with kind signature @k -> k -> Constraint@
@@ -43,54 +49,46 @@ class HiddenEmptyClass t
 class Unconstrained2 t s
 instance Unconstrained2 t s
 
-type Impossible2 t s = HiddenEmptyClass2 t s
-class HiddenEmptyClass2 t s
+class Any => Impossible2 t s
 
 
 class Unconstrained3 t s r
 instance Unconstrained3 t s r
 
-type Impossible3 t s r = HiddenEmptyClass3 t s r
-class HiddenEmptyClass3 t s r
+class Any => Impossible3 t s r
 
 
 class Unconstrained4 t s r q
 instance Unconstrained4 t s r q
 
-type Impossible4 t s r q = HiddenEmptyClass4 t s r q
-class HiddenEmptyClass4 t s r q
+class Any => Impossible4 t s r q
 
 
 class Unconstrained5 t s r q p
 instance Unconstrained5 t s r q p
 
-type Impossible5 t s r q p = HiddenEmptyClass5 t s r q p
-class HiddenEmptyClass5 t s r q p
+class Any => Impossible5 t s r q p
 
 
 class Unconstrained6 t s r q p o
 instance Unconstrained6 t s r q p o
 
-type Impossible6 t s r q p o = HiddenEmptyClass6 t s r q p o
-class HiddenEmptyClass6 t s r q p o
+class Any => Impossible6 t s r q p o
 
 
 class Unconstrained7 t s r q p o n
 instance Unconstrained7 t s r q p o n
 
-type Impossible7 t s r q p o n = HiddenEmptyClass7 t s r q p o n
-class HiddenEmptyClass7 t s r q p o n
+class Any => Impossible7 t s r q p o n
 
 
 class Unconstrained8 t s r q p o n m
 instance Unconstrained8 t s r q p o n m
 
-type Impossible8 t s r q p o n m = HiddenEmptyClass8 t s r q p o n m
-class HiddenEmptyClass8 t s r q p o n m
+class Any => Impossible8 t s r q p o n m
 
 
 class Unconstrained9 t s r q p o n m l
 instance Unconstrained9 t s r q p o n m l
 
-type Impossible9 t s r q p o n m l = HiddenEmptyClass9 t s r q p o n m l
-class HiddenEmptyClass9 t s r q p o n m l
+class Any => Impossible9 t s r q p o n m l
